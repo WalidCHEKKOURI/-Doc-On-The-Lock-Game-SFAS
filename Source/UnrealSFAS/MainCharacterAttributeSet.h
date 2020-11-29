e6807193132a5f1a96175b21de5c57e969b7776c
Walid_CHEKKOURI <virtualworld45@gmail.com>
2020-11-29 11:03:06 +0100

    Add AttributeSet for Main Character

---
+++ Source/UnrealSFAS/MainCharacterAttributeSet.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "MainCharacterAttributeSet.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class UNREALSFAS_API UMainCharacterAttributeSet : public UAttributeSet
+{
+	GENERATED_BODY()
+	
+};
