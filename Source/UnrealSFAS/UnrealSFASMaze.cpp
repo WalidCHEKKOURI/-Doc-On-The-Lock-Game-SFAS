677961f1299db4fd5a9a7c8ee60b9259afa628aa
Walid_CHEKKOURI <virtualworld45@gmail.com>
2020-12-15 21:56:35 +0100

    Add New Mazes Arrays

--- Source/UnrealSFAS/UnrealSFASMaze.cpp
+++ Source/UnrealSFAS/UnrealSFASMaze.cpp
@@ -10,6 +10,10 @@ AUnrealSFASMaze::AUnrealSFASMaze()
  	// No need to tick the maze at this point
 	PrimaryActorTick.bCanEverTick = false;
 
+
+
+
+
 }
 
 // Called when the game starts or when spawned
@@ -19,33 +23,15 @@ void AUnrealSFASMaze::BeginPlay()
 
 	if (WallMesh)
 	{
-		const int32 mazeSize = 20;
+
 		const float blockSize = 200.0f;
 		const float blockWidth = 2.0f;
 		const float blockHeight = 5.0f;
 		const float blockZPos = 50.0f;
 
-		// Array of binary values: 1 = wall, 0 = space
-		uint32 mazeArray[mazeSize] = {	0b11111111111111111111,
-										0b10100000000000000001,
-										0b10101010111111111101,
-										0b10101010001111000101,
-										0b10101010100001010001,
-										0b10101010111111111111,
-										0b10101010000000000001,
-										0b10001011111111111101,
-										0b11111000000000000101,
-										0b10001111111111111101,
-										0b10101111111111111001,
-										0b10100000000000001101,
-										0b10111011111111101001,
-										0b10100010000000101011,
-										0b10101110111110101001,
-										0b10100000100010101101,
-										0b10101110001110100001,
-										0b10111111111110111101,
-										0b10000000000000000001,
-										0b11111111111111111101 };
+
+
+
 
 		float xPos = 0.0f;
 		float yPos = 0.0f;
@@ -59,7 +45,26 @@ void AUnrealSFASMaze::BeginPlay()
 		for (int32 i = 0; i < mazeSize; i++)
 		{
 			yPos = static_cast<float>(i - (mazeSize / 2)) * blockSize;
-			mazeRow = mazeArray[i];
+
+			//Select Maze based on chosen MazeLevel
+			switch (MazeLevel)
+			{
+			case 1:
+				mazeRow = mazeArray1[i];
+				break;
+
+			case 2:
+				mazeRow = mazeArray2[i];
+				break;
+
+			case 3:
+				mazeRow = mazeArray3[i];
+				break;
+
+			default:
+				mazeRow = mazeArray1[i];
+			}
+			
 
 			for (int32 j = 0; j < mazeSize; j++)
 			{
@@ -87,4 +92,6 @@ void AUnrealSFASMaze::BeginPlay()
 	UNavigationSystemV1* NavSystem = Cast<UNavigationSystemV1>(GetWorld()->GetNavigationSystem());
 	NavSystem->Build();
 }
+
+
 
